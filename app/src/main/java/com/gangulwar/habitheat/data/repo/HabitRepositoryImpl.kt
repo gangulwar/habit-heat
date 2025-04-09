7daec9121e8cb6a5b20ae24c66952b5ac2886a35
gangulwar <aarshgangulwar23@gmail.com>
2025-04-09 17:55:17 +0530

    use interface and implementation of repo

---
+++ app/src/main/java/com/gangulwar/habitheat/data/repo/HabitRepositoryImpl.kt
@@ -0,0 +1,47 @@
+package com.gangulwar.habitheat.data.repo
+
+import com.gangulwar.habitheat.data.dao.HabitCompletionDao
+import com.gangulwar.habitheat.data.dao.HabitDao
+import com.gangulwar.habitheat.data.models.Habit
+import com.gangulwar.habitheat.data.models.HabitCompletion
+
+class HabitRepositoryImpl(
+    private val habitDao: HabitDao,
+    private val completionDao: HabitCompletionDao
+) : IHabitRepository {
+
+    override suspend fun addHabit(name: String, emoji: String?, description: String?) {
+        habitDao.insertHabit(Habit(name = name, emoji = emoji, description = description))
+    }
+
+    override suspend fun getAllHabits(): List<Habit> {
+        return habitDao.getAllHabits()
+    }
+
+    override suspend fun deleteHabit(habit: Habit) {
+        habitDao.deleteHabit(habit)
+    }
+
+    override suspend fun markHabitCompleted(habitId: Long, date: String, note: String?) {
+        completionDao.insertCompletion(
+            HabitCompletion(
+                habitId = habitId,
+                date = date,
+                note = note,
+                isCompleted = true
+            )
+        )
+    }
+
+    override suspend fun isHabitCompleted(habitId: Long, date: String): Boolean {
+        return completionDao.getCompletionForDay(habitId, date)?.isCompleted ?: false
+    }
+
+    override suspend fun getStatsForHabit(habitId: Long): List<HabitCompletion> {
+        return completionDao.getCompletionsForHabit(habitId)
+    }
+
+    override suspend fun getHabitsCompletedOn(date: String): List<HabitCompletion> {
+        return completionDao.getAllHabitsCompletedOn(date)
+    }
+}
--- app/src/main/java/com/gangulwar/habitheat/di/AppModule.kt
+++ app/src/main/java/com/gangulwar/habitheat/di/AppModule.kt
@@ -3,7 +3,10 @@ package com.gangulwar.habitheat.di
 import androidx.room.Room
 import com.gangulwar.habitheat.data.db.AppDatabase
 import com.gangulwar.habitheat.data.repo.HabitRepository
+import com.gangulwar.habitheat.data.repo.IHabitRepository
+import com.gangulwar.habitheat.presentation.viewmodel.HabitViewModel
 import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val databaseModule = module {
@@ -17,5 +20,6 @@ val databaseModule = module {
 
     single { get<AppDatabase>().habitDao() }
     single { get<AppDatabase>().completionDao() }
-    single { HabitRepository(get(), get()) }
+    single<IHabitRepository> { HabitRepositoryImpl(get(), get()) }
+    viewModel { HabitViewModel(get()) }
 }
